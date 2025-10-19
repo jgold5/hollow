c265e6042b0cb92d2caf776d9b11c1f3aef22ef1
justin <jgold885@gmail.com>
2025-10-19 21:02:30 +0000

    feat(cli): minimal command router

--- src/main.zig
+++ src/main.zig
@@ -2,21 +2,42 @@
 //! you are building an executable. If you are making a library, the convention
 //! is to delete this file and start with root.zig instead.
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // Don't forget to flush!
+pub fn main() !u8 {
+    var args = std.process.args();
+    _ = args.next();
+    const cmd = args.next().?;
+    if (std.mem.eql(u8, cmd, "build")) {
+        std.debug.print("Building now\n", .{});
+        return 0;
+    } else if (std.mem.eql(u8, cmd, "init")) {
+        std.debug.print("Initing now\n", .{});
+        return 0;
+    } else if (std.mem.eql(u8, cmd, "serve")) {
+        std.debug.print("Serving now\n", .{});
+        return 0;
+    } else if (std.mem.eql(u8, cmd, "--help")) {
+        std.debug.print("{s}\n", .{help});
+        return 0;
+    } else if (std.mem.eql(u8, cmd, "--version")) {
+        std.debug.print("Versioning now\n", .{});
+        return 0;
+    } else {
+        std.debug.print("Invalid command '{s}'\n", .{cmd});
+        return 64;
+    }
 }
+
+const help =
+    \\hollow — static site tool
+    \\USAGE: hollow <command> [options]
+    \\COMMANDS: init, build, serve
+    \\FLAGS: -h/--help, -V/--version, --verbose, --quiet
+    \\EXAMPLES:
+    \\hollow --help
+    \\hollow init
+    \\hollow build
+    \\hollow serve
+;
 
 test "simple test" {
     var list = std.ArrayList(i32).init(std.testing.allocator);
